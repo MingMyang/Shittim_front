4e9c9df07e6c24ee450c04d96a5ed0eca8fedafc
JYG <jojeayun900@gmail.com>
2023-05-18 16:54:43 +0900

    add background img

---
+++ pages/style.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const Backgorund = css`
+    background-image: url('/images/background/BG_Screen_Shittim.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+`

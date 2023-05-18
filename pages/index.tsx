165fdf616c8eafd52d62cd11869ac45d8e51a812
JYG <jojeayun900@gmail.com>
2023-05-18 16:54:27 +0900

    connect header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import * as S from "./style"
+
 import Main from "@/components/main/main"
+import Header from "@/components/header/header"
 
 export default function mainPage() {
   return (
-    <>
+    <div css={S.Backgorund}>
+      <Header />
       <Main />
-    </>
+    </div>
   )
 }

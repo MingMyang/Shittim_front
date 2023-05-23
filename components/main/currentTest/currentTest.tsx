72bc476be78c2294d501dc8dddd4ac55b1b28bce
JYG <jojeayun900@gmail.com>
2023-05-23 21:04:49 +0900

    edit test component

---
+++ components/main/currentTest/currentTest.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import * as S from "./style"
+
+function currentTest() {
+    return (
+        <div css={S.Positioner}>
+            <div css={S.BannerTitle}>종합전술시험</div>
+            <div css={S.EventName}>개최 예정</div>
+        </div>
+    )
+}
+
+export default currentTest;
---
+++ components/main/currentTest/style.ts
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react";
+
+export const Positioner = css`
+    grid-column: 2 / span 1;
+    grid-row: 3 / span 1;
+    border-radius: 5px;
+    background-color: var(--grid-box-color);
+    overflow-x: hidden;
+
+    position: relative;
+`
+
+export const BannerTitle = css`
+    width: 120px;
+    height: 35px;
+    border-radius: 5px 0px 5px 0px;
+    text-align: center;
+    line-height: 35px;
+    font-size: 15px;
+    font-weight: bold;
+    background-color: var(--grid-text-color);
+
+    position: absolute;
+    top: 0%;
+    left: 0%;
+
+    z-index: 2;
+`
+
+export const EventName = css`
+    width: 100%;
+    height: 35px;
+    border-radius: 0px 0px 5px 5px;
+    position: absolute;
+    left: 0%;
+    bottom: 0%;
+    background-color: var(--grid-text-color);
+    text-align: center;
+    line-height: 35px;
+    font-size: 15px;
+
+    z-index: 2;
+`

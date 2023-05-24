01590e747b675fad52a4d3665fb15b43eb003438
JYG <jojeayun900@gmail.com>
2023-05-24 21:16:59 +0900

    arrange code

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import * as S from "./style"
 import Main from "@/components/main/main"
 import Header from "@/components/header/header"
 
-export default function mainPage() {
+export default function index() {
   return (
     <div css={S.Backgorund}>
       <Header />

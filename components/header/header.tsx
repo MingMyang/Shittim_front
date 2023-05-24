e55c589a6ef9dc8412234ea0cde2f3fe12f69e61
JYG <jojeayun900@gmail.com>
2023-05-24 21:14:36 +0900

    add theme

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,54 @@
 /** @jsxImportSource @emotion/react */
 import * as S from "./style"
+import router, { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 function header() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const darkModeHandler = () => {
+        setIsDarkMode(!isDarkMode);
+        if (isDarkMode) {
+            document.body.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        } else {
+            document.body.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        }
+    }
+    useEffect(() => {
+        // 로컬 스토리지에서 저장된 테마 가져오기
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme === 'dark') {
+            setIsDarkMode(true);
+            document.body.setAttribute("data-theme", "dark");
+        }
+        else {
+            document.body.setAttribute("data-theme", "light");
+        }
+    }, []);
+
     return (
         <div css={S.Positioner}>
             <nav css={S.Navigation}>
-                <div css={S.FullLogo}>
+                <div css={S.FullLogo} onClick={() => router.push("/")}>
                     <div css={S.Icon}>
-                        <img css={S.LogoImg} alt='' src='images/schoolicon/School_Icon_MILLENNIUM_W.png' />
+                        <img css={S.LogoImg} alt='' src='/images/schoolicon/School_Icon_MILLENNIUM_W.png' />
                         <div css={S.IconText}>- VERITAS -</div>
                     </div>
                     <div css={S.LogoText}><span css={S.FirstText}>MILLENNIUM</span><span css={S.SecondText}>DataBase</span></div>
                 </div>
+                <div css={S.뭐로하지}>
+                    <div css={S.NavItems}>
+                        <div onClick={() => router.push("/")}>홈</div>
+                        <div onClick={() => router.push("/stdList")}>학생</div>
+                        <div>아이템</div>
+                        <div>업무</div>
+                    </div>
+                    <div></div>
+                </div>
+                <label css={S.ThemeArea}>
+                    <input checked={isDarkMode} onClick={darkModeHandler} css={S.ThemeSwitch} type="checkbox" />
+                </label>
             </nav>
         </div>
     )
--- components/header/style.ts
+++ components/header/style.ts
@@ -2,21 +2,27 @@ import { css } from "@emotion/react";
 
 export const Positioner = css`
     width: 100vw;
-    height: 7vh;
-    background-color: rgba(51, 51, 51, 0.7);
+    height: 65px;
+    padding: 0vh 10vw 0vh 10vw;
+    background-color: var(--bg-color);
+
+    display: flex;
+    justify-content: center;
+    position: fixed;
 `
 
 export const Navigation = css`
-    width: 100%;
+    width: 1500px;
     height: 100%;
-    padding: 0px 10vw 0px 10vw;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
 `
 
 export const FullLogo = css`
-    width: auto;
+    width: 215px;
     height: 100%;
+    cursor: pointer;
 `
 
 export const Icon = css`
@@ -60,4 +66,81 @@ export const FirstText = css`
 
 export const SecondText = css`
     font-size: 30px;
+`
+
+
+export const 뭐로하지 = css`
+    width: calc(100% - 415px);
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const NavItems = css`
+    width: 320px;
+    height: 100%;
+    font-size: 18px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    div{
+        width: 75px;
+        height: 40px;
+        border-radius: 5px;
+        cursor: pointer;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        :hover{
+            background-color: var(--bg-color);
+        }
+    }
+`
+
+export const NavItem = css`
+    background-color: var(--nav-select-color);
+`
+
+export const ThemeArea = css`
+    width: 100px;
+    height: 100%;
+    cursor: pointer;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const ThemeSwitch = css`
+    width: 60px;
+    height: 30px;
+    background-color: rgba(255, 255, 255, 0);
+    border: 5px solid var(--theme-color);
+    border-radius: 50px;
+    appearance: none;
+    cursor: pointer;
+    position: relative;
+    ::before{
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: black;
+        transform: scale(0.8);
+        transition: left 250ms linear;
+    }
+
+    :checked {
+        background-color: rgba(255, 255, 255, 0);
+        border-color: var(--theme-color);
+    }
+    :checked::before {
+        background-color: white;
+        left: 29px;
+    }
 `

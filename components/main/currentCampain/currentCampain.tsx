1f942ef00b41fafc233a798ca6d14a911c040c64
JYG <jojeayun900@gmail.com>
2023-06-08 15:38:35 +0900

    update test

--- components/main/currentCampain/currentCampain.tsx
+++ components/main/currentCampain/currentCampain.tsx
@@ -5,8 +5,8 @@ function currentCampain() {
 
     return (
         <div css={S.Positioner}>
-            <div css={S.BannerTitle}>진행중인 캠페인</div>
-            <div css={S.EventName}>개최 예정</div>
+            <div css={S.BannerTitle}>버전 확인</div>
+            <div css={S.EventName}>최신 버전입니다</div>
         </div>
     )
 }
